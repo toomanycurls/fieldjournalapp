f7d29d6767126f0855488d6bcc117a702265c961
Rose Becerra <55454504+toomanycurls@users.noreply.github.com>
2026-03-25 08:53:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-export const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+export const SUPABASE_URL = 'https://pmkcygvamoknfyxswehw.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_jDQG4bsMTBWpSwTg0mWAGQ_h9EMWQu-';
